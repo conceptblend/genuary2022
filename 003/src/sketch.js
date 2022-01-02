37980ee31e100ecba4b5d532ae7c98aa90302246
conceptblend <the.concept.blend@gmail.com>
2022-01-02 15:01:53 -0700

    003: initial commit of template

---
+++ 003/src/sketch.js
@@ -0,0 +1,88 @@
+// PARAMETER SETS
+const PARAMS = [
+  {
+    name: "set one",
+    seed: "hello world",
+    width: 540,
+    height: 540,
+    fps: 30,
+    duration: 30 * 10, // no unit (frameCount by default; sometimes seconds or frames or whatever)
+    a: 0,
+    b: 0,
+  },
+];
+
+// PARAMETERS IN USE
+const P = PARAMS[0];
+
+// VIDEO
+const EXPORTVIDEO = P.exportVideo ?? false; // set to `false` to not export
+const FPS = P.fps;
+const DURATION = P.duration;
+let cnvsrecorder;
+let isRecording = false;
+
+function setup() {
+  createCanvas( P.width, P.height );
+  angleMode( DEGREES );
+  colorMode( RGB, 255 );
+  
+  noStroke();
+  
+  Math.seedrandom( P.seed );
+  
+  frameRate( FPS );
+
+  // noLoop();
+}
+
+
+function draw() {
+  background(0);
+
+  // DO YOUR DRAWING HERE!
+  noFill();
+  stroke( 255, 128, 32 );
+  rect( 20, 20, 30, 30 );
+
+  if (EXPORTVIDEO) {
+    if (!isRecording) {
+      cnvsrecorder = new CanvasRecorder(FPS);
+      cnvsrecorder.start();
+      isRecording = true;
+      console.log('Recording...');
+    }
+    // Example to end automatically after 361 frames to get a full loop
+    if (frameCount > DURATION) {
+      cnvsrecorder.stop(`${getName()}`);
+      noLoop();
+      console.log('Done.');
+    }
+  }
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function getName() {
+  // Encode the parameters into the filename
+  // let params = window.btoa(JSON.stringify(P));
+  let params = MD5( JSON.stringify(P) );
+  return `${P.name}-${params}-${new Date().toISOString()}`;
+}
+
+function saveImage( ext ) {
+  save(`${ getName() }.${ ext ?? 'jpg' }`);
+}
+
+function saveConfig() {
+  saveJSON( P, `${getName()}-config.json` );
+}
+
+function downloadOutput() {
+  saveImage();
+  saveConfig();
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
