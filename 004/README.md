3d3fd35b9d7b781ef101de18fa63ffb78ab34982
conceptblend <the.concept.blend@gmail.com>
2022-01-04 20:59:13 -0700

    004: feat: final and documented

--- 004/README.md
+++ 004/README.md
@@ -1,7 +1,42 @@
 # Genuary 004
 
+JAN.4 (credit: Alexis André) The next next Fidenza.
 
+[Tyler Hobbs’ Fidenza](https://tylerxhobbs.com/fidenza)
 
+I had a bunch of fun figuring out some of the basics of what went into Fidenza. I'm a loooooong way from creating something this like (no collision detection, no spacing, colour palettes could use a lot of work) but it was enjoyable none-the-less.
+
+There's a good chance I'll come back to play with this some more.
+
+-----
+
+#### Black and white
+
+![01](./doc/01_IG_notfidenza-ef011d50f0db07f2a5dc721860b7438a-2022-01-05T03_26_05.412Z.jpg)
+
+![02](./doc/02_IG_notfidenza-ba4387b46bfbea10dbac29d0724e26d7-2022-01-05T03_25_45.602Z.jpg)
+
+-----
+
+#### Animaux
+![11](./doc/11_IG_notfidenza2-ff0a99fcaa351761eed24526f5bfec09-2022-01-05T01_13_15.718Z.jpg)
+![12](./doc/12_IG_notfidenza2-ff0a99fcaa351761eed24526f5bfec09-2022-01-05T01_11_45.518Z.jpg)
+
+-----
+
+![21](./doc/21_IG_notfidenza-50a2e559386affbf4811a6b95600d41d-2022-01-05T01_31_33.337Z.jpg)
+![22](./doc/22_IG_notfidenza-7e979e8abb6568536a7825b6b4263171-2022-01-04T20_00_29.535Z.jpg)
+
+-----
+
+![31](./doc/31_IG_notfidenza-smushy-e88d6c6203f794573a5abf5ea06dbca4-2022-01-05T03_09_56.739Z.jpg)
+
+![32](./doc/32_IG_notfidenza-smushy-d8ea8552eac90d0b796d9883770f7580-2022-01-05T03_21_43.221Z.jpg)
+
+-----
+
+![41](./doc/41_IG_notfidenza-smushy-6bddd67e7c05cea913b3aa4bfd9e4581-2022-01-05T03_16_10.238Z.jpg)
+![42](./doc/42_IG_notfidenza-smushy-6c913de03c792585d2214b8bf0e9ae73-2022-01-05T03_18_46.431Z.jpg)
 
 -----
 
--- 004/src/sketch.js
+++ 004/src/sketch.js
@@ -22,26 +22,78 @@ const PARAMS = [
   {
     name: "notfidenza",
     seed: "this is not a fidenza",
-    width: 540,
-    height: 540,
+    width: 540*2,
+    height: 540*2,
     exportVideo: !true,
     exportImages: !true,
     animated: !true,
     fps: 4,
     duration: 4 * 10, // no unit (frameCount by default; sometimes seconds or frames or whatever)
-    numSegmentsPerLine: 16,
-    lineSegmentLength: 24,
-    segmentDensity: 0.8,
-    gridSize: 12,
+    numSegmentsPerLine: 24*2,
+    lineSegmentLength: 24*2,
+    segmentDensity: 0.2,
+    gridSize: 24,
     paletteStart: 162,//255,//162,
-    paletteSize: 64,
+    paletteSize: 24,
     paletteRange: 120,
-    strokeMax: 12,
+    strokeMax: 12*2,
+    noiseStart: 73.1415926, // 285937
+    noiseIncrement: 0.001,
   },
+  {
+    name: "notfidenza2",
+    seed: "this is not a fidenza",
+    width: 540*2,
+    height: 540*2,
+    exportVideo: !true,
+    exportImages: !true,
+    animated: !true,
+    fps: 4,
+    duration: 4 * 10, // no unit (frameCount by default; sometimes seconds or frames or whatever)
+    numSegmentsPerLine: 24*2,
+    lineSegmentLength: 24*2,
+    segmentDensity: 0.55,
+    gridSize: 16,
+    paletteStart: 32,//255,//162,
+    paletteSize: 8,
+    paletteRange: 180,
+    strokeMax: 12*3,
+    noiseStart: 87,//3.1415926, // 285937
+    noiseIncrement: 0.001,
+    continuousSegmentColour: !true,
+  },
+  {
+    "name": "notfidenza-smushy",
+    "seed": "this is not a fidenza",
+    "width": 1080,
+    "height": 1080,
+    "exportVideo": false,
+    "exportImages": false,
+    "animated": false,
+    "fps": 4,
+    "duration": 40,
+    "numSegmentsPerLine": 24,
+    "lineSegmentLength": 16,
+    "segmentDensity": 0.15,
+    "gridSize": 32,
+    "paletteStart": 65,
+    "paletteSize": 8,
+    "paletteRange": 180,
+    "strokeMax": 36,
+    "noiseStart": 87,
+    "noiseIncrement": 0.008,
+    "continuousSegmentColour": false,
+    blackAndWhiteMode: true,
+  }  
 ];
 
+/**
+ * 0.00001, -> straight lines?!?
+ * 0.0001,  -> bowed lines
+ */
+
 // PARAMETERS IN USE
-const P = PARAMS[ PARAMS.length - 1 ];
+const P = PARAMS[ PARAMS.length - 2 ];
 
 // VIDEO
 const EXPORTVIDEO = P.exportVideo ?? false; // set to `false` to not export
@@ -60,15 +112,22 @@ const cStrokeBase = {
   brightness: 30
 }; // HSB( 360, 100, 100 );
 
-const cFillBase = {
-  hue: 0,
-  saturation: 40,
-  brightness: 80
-}; // HSB( 360, 100, 100 );
+// BONUS: B&W EDITION
+const cFillBase = P.blackAndWhiteMode ?
+  {
+    hue: 0,
+    saturation: 0,
+    brightness: 100
+  }  // HSB( 360, 100, 100 );
+  :
+  {
+    hue: 0,
+    saturation: 60,
+    brightness: 80
+  }; // HSB( 360, 100, 100 );
 
 let cStroke;
 let cFills = [];
-
 
 function drawPath1( points ) {
   stroke( 255 );
@@ -80,45 +139,104 @@ function drawPath1( points ) {
 //
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //
-function drawPath( points ) {
+function drawPath2( points ) {
   // stroke( Math.floor( Math.random() * 255 ) );
   stroke( cFills[ Math.floor( Math.random() * cFills.length ) ]);
   noFill();
 
-  for( let n = 1, len = points.length; n < len; n++ ) {
+  // for( let n = 1, len = points.length; n < len; n++ ) {
+  let n = 1, len = points.length;
+  while ( n < len ) {
 
     if ( Math.random() < P.segmentDensity ) {
       let s = Math.floor( 1 + Math.random() * P.strokeMax );
+      let l = Math.floor( 2 + Math.random() * 5 );
       push();
       stroke( 0, 80, 30 );
       strokeWeight( s+2 );
-      line( points[ n-1 ].x, points[ n-1 ].y, points[ n ].x, points[ n ].y );
+      beginShape();
+      vertex(points[ n-1 ].x, points[ n-1 ].y);
+      for( let ss = 0; ss < l && (n+ss) < len; ss++ ) {
+        // line( points[ n-1+ss ].x, points[ n-1+ss ].y, points[ n+ss ].x, points[ n+ss ].y );
+        vertex( points[ n+ss ].x, points[ n+ss ].y );
+      }
+      endShape();
       pop();
-      
+
       // stroke( cFills[ Math.floor( Math.random() * cFills.length ) ]);
       strokeWeight( s );
-      line( points[ n-1 ].x, points[ n-1 ].y, points[ n ].x, points[ n ].y );
+      beginShape();
+      vertex(points[ n-1 ].x, points[ n-1 ].y);
+      for( ss = 0; ss < l && (n+ss) < len; ss++ ) {
+        // line( points[ n-1+ss ].x, points[ n-1+ss ].y, points[ n+ss ].x, points[ n+ss ].y );
+        vertex( points[ n+ss ].x, points[ n+ss ].y );
+      }
+      endShape();
     }
+    n += ss;
+  }
+}
+//
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//
+function drawPath( points ) {
+  // stroke( Math.floor( Math.random() * 255 ) );
+  P.continuousSegmentColour && stroke( cFills[ Math.floor( Math.random() * cFills.length ) ]);
+  noFill();
 
+  let n = 1,
+      len = points.length,
+      offset = 4,
+      ss = 0;
+  while ( n < len ) {
+
+    if ( Math.random() < P.segmentDensity ) {
+      let s = Math.floor( 2 + Math.random() * P.strokeMax );
+      let l = Math.floor( 2 + Math.random() * 5 );
+      
+      strokeWeight( s );
+
+      push();
+      P.blackAndWhiteMode ? stroke( 0, 0, 0 ) : stroke( 0, 80, 30 );
+      beginShape();
+      curveVertex(points[ n-1 ].x + offset, points[ n-1 ].y + offset); // repeat the start point
+      curveVertex(points[ n-1 ].x + offset, points[ n-1 ].y + offset);
+      for( ss = 0; ss < l && (n+ss) < len; ss++ ) {
+        curveVertex( points[ n + ss ].x, points[ n + ss ].y + offset);
+      }
+      curveVertex( points[ n + ss - 1 ].x + offset, points[ n + ss - 1 ].y + offset);// repeat the last point
+      endShape();
+      pop();
+
+      !!!P.continuousSegmentColour && stroke( cFills[ Math.floor( Math.random() * cFills.length ) ]);
+
+      beginShape();
+      curveVertex(points[ n-1 ].x, points[ n-1 ].y); // repeat the start point
+      curveVertex(points[ n-1 ].x, points[ n-1 ].y);
+      for( ss = 0; ss < l && (n+ss) < len; ss++ ) {
+        curveVertex( points[ n + ss ].x, points[ n + ss ].y );
+      }
+      curveVertex( points[ n + ss - 1 ].x, points[ n + ss - 1 ].y );// repeat the last point
+      endShape();
+    }
+    n += ss;
   }
 }
 //
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 //
 
-
 let paths = [];
 
 function setup() {
   createCanvas( P.width, P.height );
   angleMode( DEGREES );
-  // colorMode( RGB, 255 );
   colorMode( HSB, 360, 100, 100, 1 );
 
-  strokeCap( ROUND );
+  strokeCap( SQUARE );
+  // strokeCap( ROUND );
   // strokeCap( PROJECT );
-  // stroke( cStroke );
-  // strokeWeight( 2 );
+  strokeJoin( ROUND );
   
   Math.seedrandom( P.seed );
   
@@ -149,7 +267,7 @@ function buildAPath( sx, sy, steps ) {
       _path.addPoint( createVector( sx, sy ) );
     } else {
       prev = _path.points[ 0 ]; // 0 because I'm unshifting to the "top" of the array
-      let val = getFlowFieldValue( prev.x, prev.y ) * 360;
+      let val = getFlowFieldValue( prev.x, prev.y, P.noiseIncrement ) * 360;
       stepVec.setHeading( val * Math.PI / 180 ); // Super annoying. Only implemented in RADIANS. Log a bug?
       _path.addPoint( p5.Vector.add( prev, stepVec ) );
     }
@@ -159,19 +277,18 @@ function buildAPath( sx, sy, steps ) {
 
 
 function draw() {
-  // background( 0 );
+  // background( 0, 80, 70 );
   background( 0, 80, 10 );
 
   paths = [];
 
-  for( let y = 1; y < GRIDSIZE; y++ ) {
-    for( let x = 1; x < GRIDSIZE; x++ ) {
+  for( let y = 0; y < GRIDSIZE + 1; y++ ) {
+    for( let x = 0; x < GRIDSIZE + 1; x++ ) {
       paths.push( buildAPath( x * GRIDSTEP, y * GRIDSTEP, P.numSegmentsPerLine ) );
     }
   }
 
-  // flowField( P.width, P.height );
-  paths.forEach( path => path.draw() );
+  paths.reverse().forEach( path => path.draw() );
 
   if ( P.animated && P.exportImages ) {
     saveImage();
@@ -219,36 +336,12 @@ function downloadOutput() {
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
-function flowField( w, h ) {
-  const cellSize = 8;
-  const rows = h / cellSize;
-  const cols = w / cellSize;
-  const inc = 0.001;
-  let xOff = 0,
-      yOff = 0,
-      zOff = 8 * frameCount * inc;
-
-  noStroke();
-  for (let y = 0; y < rows; y++) {
-    yOff = y * inc;
-    xOff = 0;
-    for (let x = 0; x < cols; x++ ) {
-      xOff += inc;
-      let c = noise( xOff, yOff, zOff );
-      fill( c * 255 );
-      rect( x * cellSize, y * cellSize, cellSize, cellSize )
-    }
-  }
-}
-
-function getFlowFieldValue( x, y ) {
+function getFlowFieldValue( x, y, _inc ) {
   // const cellSize = 8;
-  const inc = 0.005;
+  const inc = _inc ?? 0.005;
   let xOff = x * inc,
       yOff = y * inc,
-      zOff = 8 * (frameCount + 1) * inc;
+      zOff = P.noiseStart + 8 * (frameCount + 1) * inc;
 
   return noise( xOff, yOff, zOff );
 }
