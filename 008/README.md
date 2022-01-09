b019bbc8eb9a57728a0ff3a9ada9bc864865967f
conceptblend <the.concept.blend@gmail.com>
2022-01-09 10:32:59 -0700

    008: feat: final output and docs

--- 008/README.md
+++ 008/README.md
@@ -4,9 +4,25 @@ JAN.8 (credit: Bruce Holmer): Single curve only.
 
 My prompts:
 
-- bezier curve
+- bezier curve -> •• cubic-bezier( 0, .95 , .92, .69 ) ••
 - animation
 - timing curve
-- blocks
+- ~~blocks~~
 - black and white
 
+-----
+
+### Animated output
+
+![animated GIF the output](./doc/01--box-topples-5113592ed27a54eee30a7413cda4ad16-2022-01-09T17_07_58.980Z.gif)
+
+-----
+### A subset the outputs
+#### 02
+
+![02](./doc/02--box-topples-53cf7120fcff45a8d60e58e6e9b8cce0-2022-01-09T17_11_27.873Z.jpg)
+
+#### 03
+![03](./doc/03--box-topples-53cf7120fcff45a8d60e58e6e9b8cce0-2022-01-09T17_11_51.802Z.jpg)
+#### 04
+![04](./doc/04--box-topples-53cf7120fcff45a8d60e58e6e9b8cce0-2022-01-09T17_13_18.449Z.jpg)
--- 008/src/sketch.js
+++ 008/src/sketch.js
@@ -25,16 +25,30 @@ const PARAMS = [
     duration: 24 * 10, // no unit (frameCount by default; sometimes seconds or frames or whatever)
     exportVideo: false,
     isAnimated: !false,
-    blockSize: 9*MULT,
+    blockSize: 4*MULT,
     sinMultiplier: 3,
     aniCurveHandles: [[ 1, 0 ],[ 0, 0 ]],
     // aniCurveHandles: [[ 1, 0 ],[ 0, 1 ]],
     // aniCurveHandles: [[ .42, 0 ],[ .58, 1 ]],
   },
+  {
+    name: "box-topples",
+    seed: "hello world",
+    width: 540*MULT,
+    height: 540*MULT,
+    fps: 16,
+    duration: 721, // no unit (frameCount by default; sometimes seconds or frames or whatever)
+    exportVideo: !true,
+    isAnimated: true,
+    blockSize: 27*MULT,
+    sinMultiplier: 3,
+    aniCurveHandles: [[ 0, .95 ],[ .92, .69 ]],
+    // aniCurveHandles: [[ 1, 0 ],[ 0, 0 ]],
+  },
 ];
 
 // PARAMETERS IN USE
-const P = PARAMS[ PARAMS.length - 2 ];
+const P = PARAMS[ PARAMS.length - 1 ];
 
 // VIDEO
 const EXPORTVIDEO = P.exportVideo ?? false; // set to `false` to not export
@@ -43,6 +57,17 @@ const DURATION = P.duration;
 let cnvsrecorder;
 let isRecording = false;
 
+// DRAWING
+const aniCurve = [
+  new Point( 0, 0 ),
+  new Point( P.aniCurveHandles[0][0], P.aniCurveHandles[0][1] ),
+  new Point( P.aniCurveHandles[1][0], P.aniCurveHandles[1][1] ),
+  new Point( 1, 1 ),
+];
+
+const MIN_W = P.width * 0.1;
+const MAX_W = P.width * 0.9;
+
 function setup() {
   createCanvas( P.width, P.height );
   angleMode( DEGREES );
@@ -50,6 +75,7 @@ function setup() {
   
   noStroke();
   fill( 255 );
+  // fill( 0 );
   
   Math.seedrandom( P.seed );
   
@@ -58,39 +84,36 @@ function setup() {
   if ( !EXPORTVIDEO && !P.isAnimated ) noLoop();
 }
 
-
-const aniCurve = [
-  new Point( 0, 0 ),
-  new Point( P.aniCurveHandles[0][0], P.aniCurveHandles[0][1] ),
-  new Point( P.aniCurveHandles[1][0], P.aniCurveHandles[1][1] ),
-  new Point( 1, 1 ),
-];
-
-let x = 0;
-let lastPosition;
-let T = 0;
-const STEPS = 60;
-const STEPSIZE = P.width / STEPS;
+function mapToScale( val, aLow, aHigh, bLow, bHigh ) {
+  return ( val / (aHigh - aLow) * (bHigh - bLow )) + bLow;
+}
 
 function draw() {
-  background(0);
+  background( 0, 0, 0, 20 );
+  // background( 255, 255, 255, 20 );
 
-  // T = (( frameCount % STEPS ) / STEPS );
-  const max = Math.min(
+  let x = 0,
+      T = 0;
+
+  /** STAGGERED START */
+  let max = Math.min(
     ( frameCount * 0.5 ) * P.blockSize,
     P.height
   );
+  /** UNIFORM START */
+  // let max = P.height;
 
-  for( let y = 0; y < max; y += P.blockSize ) {
-    T = 0.5 + 0.5 * sin( (frameCount + y) * P.sinMultiplier );
-    // T = 0.5 + ((frameCount*P.sinMultiplier) + y - P.height * 0.5) / P.height;
+  for( let y = P.blockSize; y < max - P.blockSize; y += P.blockSize ) {
+    T = 0.5 + 0.5 * cos( (frameCount + y) * P.sinMultiplier );
 
     let aniTiming = cubicBezier(aniCurve[0], aniCurve[1], aniCurve[2], aniCurve[3], T);
 
-    x = aniTiming.y * (P.width - P.blockSize);
-    x %= P.width;
+    x = MIN_W + aniTiming.y * ( MAX_W - P.blockSize - MIN_W );    
 
-    rect( y, x, P.blockSize, P.blockSize );
+    let d = mapToScale( aniTiming.y, 0, 1, P.blockSize*.25, P.blockSize );
+    // rect( x, y, d, d ); // DYNAMIC SIZE
+    circle( x+P.blockSize*0.5, y+P.blockSize*0.5, d, d ); // DYNAMIC SIZE
+    // rect( x, y, P.blockSize, P.blockSize ); // STATIC SIZE
   }
 
 
